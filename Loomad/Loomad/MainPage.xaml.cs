9d465188a73e7ee4a1d34508cfbaa26ed3a72551
opilane <opilane@HP-CZC6297B82>
2020-10-16 12:15:37 +0300

    loomad

--- Loomad/Loomad/MainPage.xaml.cs
+++ Loomad/Loomad/MainPage.xaml.cs
@@ -10,9 +10,26 @@ namespace Loomad
 {
     public partial class MainPage : TabbedPage
     {
+        Button btn;
+
         public MainPage()
+        { btn = new Button();
+            btn.Clicked += Btn_Clicked1;
+            StackLayout stackLayout = new StackLayout()
+
+            {
+                Children = { btn }
+            };stackLayout.Orientation = StackOrientation.Vertical;
+            }
+        private async void Btn_Clicked1(object sender, EventArgs e)
+        {
+
+            string answer = await DisplayActionSheet("выьбрать животное", "отменить", null);
+            if (answer == "")
+
         {
             InitializeComponent();
+        }
         }
     }
 }
